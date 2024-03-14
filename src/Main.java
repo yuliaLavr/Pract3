fed57e103e647d7695242d79851fae7486b88eba
Vasia Pupkin <vpupkin@mail.com>
2024-03-14 22:47:15 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        //Завдання 1:
+        for (int i = 500; i <= 650; i = i + 10) {
+            System.out.println(i + " ");
+        }
+        //Завдання 2:
+        for (int a = 2; a < 500; a = 2 * a - 1) {
+            System.out.println(a);
+        }
+        //Завдання 3:
+        int d;
+        d = 10;
+        for (int i1 = 1; i1 <= d; i1++) {
+            if(d%i1==0){
+                System.out.println(i1);
+            }
+        }
+        //Завдання 4(for):
+        int n,f;
+        f = 1;
+        n = 10;
+        for(int i=1; i<=n; i++) {
+            f = f*i;
+        }
+        System.out.println(n+"! = "+f);
+        //Завдання 4(while):
+        int n2, f2;
+        f2=1;
+        n2=9;
+        int e=1;
+        while(e<=n2){
+            e++;
+            f2=f2*e;
+        }
+        System.out.println(n2+"! = "+f2);
+    }
+}
